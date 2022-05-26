7790f10ef34ddd40b54a5668857a069155d10f70
htookhantlinn <htookhantlin@ucsy.edu.mm>
2022-05-26 16:50:06 +0630

    be better

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker;
 use Illuminate\Support\Str;
+use App\Helpers\Utilities;
 
 class StudentSeeder extends Seeder
 {
